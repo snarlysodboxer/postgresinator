4286c17517f2bb4671bc6676ad34e83bf371efab
david amick <github@davidamick.com>
2014-10-30 20:42:52 +0000

    Hack to prevent methods getting overwritten by similar gems, (fix this later.) Version bump 0.0.3.

--- lib/postgresinator/pg.rb
+++ lib/postgresinator/pg.rb
@@ -35,21 +35,21 @@ namespace :pg do
         config_file_changed = false
         cluster.image.config_files.each do |config_file|
           next if config_file == "recovery.conf"
-          if config_file_differs?(cluster, server, config_file)
+          if pg_config_file_differs?(cluster, server, config_file)
             warn "Config file #{config_file} on #{server.domain} is being updated."
             Rake::Task['pg:install_config_file'].invoke(server.domain, config_file)
             Rake::Task['pg:install_config_file'].reenable
             config_file_changed = true
           end
         end
-        unless container_exists?(server)
+        unless pg_container_exists?(server)
           # the create_container task's prerequisite task :ensure_config_files is
           #   for manual use of create_container, so here we clear_prerequisites.
           Rake::Task['pg:create_container'].clear_prerequisites
           Rake::Task['pg:create_container'].invoke(server.domain)
           Rake::Task['pg:create_container'].reenable
         else
-          unless container_is_running?(server)
+          unless pg_container_is_running?(server)
             Rake::Task['pg:start_container'].invoke(server.domain)
             Rake::Task['pg:start_container'].reenable
           else
@@ -64,18 +64,18 @@ namespace :pg do
         # sleep to allow postgres to start up before running subsequent commands against it
         sleep 3
         if server.master
-          unless role_exists?(cluster, server, "replicator")
+          unless pg_role_exists?(cluster, server, "replicator")
             info "Creating role 'replicator' #{server.domain}"
             Rake::Task['pg:create_role'].invoke(server.domain, "replicator")
             Rake::Task['pg:create_role'].reenable
           end
           cluster.databases.each do |database|
-            unless role_exists?(cluster, server, database.role)
+            unless pg_role_exists?(cluster, server, database.role)
               info "Creating role #{database.role} on #{server.domain}"
               Rake::Task['pg:create_role'].invoke(server.domain, database.role)
               Rake::Task['pg:create_role'].reenable
             end
-            unless database_exists?(cluster, server, database)
+            unless pg_database_exists?(cluster, server, database)
               info "Creating database #{database.name} on #{server.domain}"
               Rake::Task['pg:create_database'].invoke(server.domain, database.name)
               Rake::Task['pg:create_database'].reenable
@@ -102,9 +102,9 @@ namespace :pg do
     cluster = @cluster
     server = cluster.servers.select { |s| s.domain == args.domain }.first
     on "#{cluster.ssh_user}@#{server.domain}" do
-      if container_exists?(server)
+      if pg_container_exists?(server)
         info "#{server.container_name} exists on #{server.domain}"
-        if container_is_running?(server)
+        if pg_container_is_running?(server)
           info ""
           info "#{server.container_name} is running on #{server.domain}"
           info ""
@@ -134,8 +134,8 @@ namespace :pg do
     on "#{cluster.ssh_user}@#{server.domain}" do
       if server.master
         clean = ""
-        unless database_empty?(cluster, server, database)
-          if confirm_database_overwrite?(server, database); clean = "--clean"; else exit(0); end
+        unless pg_database_empty?(cluster, server, database)
+          if pg_confirm_database_overwrite?(server, database); clean = "--clean"; else exit(0); end
         end
         execute("docker", "run", "--rm",
           "--volume", "/tmp:/tmp:rw",
@@ -157,7 +157,7 @@ namespace :pg do
     server = cluster.servers.select { |s| s.master }.first
     on "#{cluster.ssh_user}@#{server.domain}" do
       if server.master
-        confirm_file_overwrite?(server, args.dump_file) if file_exists?("/tmp/#{args.dump_file}")
+        pg_confirm_file_overwrite?(server, args.dump_file) if pg_file_exists?("/tmp/#{args.dump_file}")
         execute("docker", "run", "--rm",
           "--volume", "/tmp:/tmp:rw",
           "--entrypoint", "/bin/bash",
@@ -186,7 +186,7 @@ namespace :pg do
     on "#{cluster.ssh_user}@#{server.domain}" do
       cluster.image.config_files.each do |config_file|
         next if config_file == "recovery.conf"
-        if config_file_differs?(cluster, server, config_file)
+        if pg_config_file_differs?(cluster, server, config_file)
           Rake::Task['pg:install_config_file'].invoke(server.domain, config_file)
           Rake::Task['pg:install_config_file'].reenable
         end
@@ -200,7 +200,7 @@ namespace :pg do
     master_server = cluster.servers.select { |s| s.master }.first
     unless server.master
       on "#{cluster.ssh_user}@#{master_server.domain}" do
-        fatal "Master must be running before creating a slave" and raise unless container_is_running?(master_server)
+        fatal "Master must be running before creating a slave" and raise unless pg_container_is_running?(master_server)
       end
     end
     on "#{cluster.ssh_user}@#{server.domain}" do
@@ -208,7 +208,7 @@ namespace :pg do
       as 'root' do
         fatal_message = "#{server.data_path} on #{server.domain} is not empty, cannot continue! " +
           "You'll need to delete those files by hand. Be sure you are not deleting important data!"
-        fatal fatal_message and raise if files_in_data_path?(server)
+        fatal fatal_message and raise if pg_files_in_data_path?(server)
         execute("mkdir", "-p", server.data_path) unless test("test", "-d", server.data_path)
         execute("chown", "-R", "#{cluster.image.postgres_uid}:#{cluster.image.postgres_gid}", server.data_path)
         execute("chmod", "700", server.data_path)
@@ -233,7 +233,7 @@ namespace :pg do
       warn "Restarting a running container named #{server.container_name}"
       execute("docker", "restart", server.container_name)
       sleep 2
-      fatal stay_running_message(server) and raise unless container_is_running?(server)
+      fatal pg_stay_running_message(server) and raise unless pg_container_is_running?(server)
     end
   end
 
@@ -244,7 +244,7 @@ namespace :pg do
       warn "Starting an existing but non-running container named #{server.container_name}"
       execute("docker", "start", server.container_name)
       sleep 2
-      fatal stay_running_message(server) and raise unless container_is_running?(server)
+      fatal pg_stay_running_message(server) and raise unless pg_container_is_running?(server)
     end
   end
 
@@ -268,7 +268,7 @@ namespace :pg do
         # TODO: get this recovery.conf dependancy out of here?
         path = args.config_file == "recovery.conf" ? server.data_path : server.conf_path
         execute("mkdir", "-p", path) unless test("test", "-d", path)
-        generated_config_file = generate_config_file(cluster, server, args.config_file)
+        generated_config_file = pg_generate_config_file(cluster, server, args.config_file)
         upload! StringIO.new(generated_config_file), "/tmp/#{args.config_file}"
         execute("mv", "/tmp/#{args.config_file}", "#{path}/#{args.config_file}")
         execute("chown", "-R", "#{cluster.image.postgres_uid}:#{cluster.image.postgres_gid}", path)
@@ -383,7 +383,7 @@ namespace :pg do
       as "root" do
         inner_server_crt = "#{cluster.image.data_path}/server.crt"
         outer_server_crt = "#{server.data_path}/server.crt"
-        unless file_exists?(outer_server_crt)
+        unless pg_file_exists?(outer_server_crt)
           execute("docker", "run",
             "--rm", "--user", "root", "--entrypoint", "/bin/ln",
             "--volume", "#{server.data_path}:#{cluster.image.data_path}:rw",
@@ -393,7 +393,7 @@ namespace :pg do
         end
         inner_server_key = "#{cluster.image.data_path}/server.key"
         outer_server_key = "#{server.data_path}/server.key"
-        unless file_exists?(outer_server_key)
+        unless pg_file_exists?(outer_server_key)
           execute("docker", "run",
             "--rm", "--user", "root", "--entrypoint", "/bin/ln",
             "--volume", "#{server.data_path}:#{cluster.image.data_path}:rw",
@@ -419,18 +419,21 @@ namespace :pg do
 
   private
 
-    def stay_running_message(server)
+    # Temporarily added 'pg_' to the beginning of each of these methods to avoid
+    #   getting them overwritten by other gems with methods with the same names, (E.G. nginxinator.)
+    ## TODO Figure out how to do this the right or better way.
+    def pg_stay_running_message(server)
       "Container #{server.container_name} on #{server.domain} did not stay running more than 2 seconds"
     end
 
-    def files_in_data_path?(server)
+    def pg_files_in_data_path?(server)
       test("[", "\"$(ls", "-A", "#{server.data_path})\"", "]")
     end
 
-    def config_file_differs?(cluster, server, config_file)
-      generated_config_file = generate_config_file(cluster, server, config_file)
+    def pg_config_file_differs?(cluster, server, config_file)
+      generated_config_file = pg_generate_config_file(cluster, server, config_file)
       as 'root' do
-        if file_exists?("#{server.conf_path}/#{config_file}")
+        if pg_file_exists?("#{server.conf_path}/#{config_file}")
           capture("cat", "#{server.conf_path}/#{config_file}").chomp != generated_config_file.chomp
         else
           true
@@ -438,14 +441,14 @@ namespace :pg do
       end
     end
 
-    def generate_config_file(cluster, server, config_file)
+    def pg_generate_config_file(cluster, server, config_file)
       @cluster      = cluster # needed for ERB
       @server       = server  # needed for ERB
       template_path = File.expand_path("templates/#{config_file}.erb")
       ERB.new(File.new(template_path).read).result(binding)
     end
 
-    def role_exists?(cluster, server, role)
+    def pg_role_exists?(cluster, server, role)
       test("echo", "\"SELECT", "*", "FROM", "pg_user", "WHERE", "usename", "=", "'#{role}';\"", "|",
         "docker", "run", "--rm", "--interactive",
         "--entrypoint", "/bin/bash",
@@ -455,7 +458,7 @@ namespace :pg do
         "grep", "-q", "'#{role}'")
     end
 
-    def database_exists?(cluster, server, database)
+    def pg_database_exists?(cluster, server, database)
       test "docker", "run", "--rm",
         "--entrypoint", "/bin/bash",
         "--volumes-from", server.container_name,
@@ -464,21 +467,21 @@ namespace :pg do
         "cut", "-d\\|", "-f1", "|", "grep", "-w", "#{database.name}'"
     end
 
-    def container_exists?(server)
+    def pg_container_exists?(server)
       test "docker", "inspect", server.container_name, ">", "/dev/null"
     end
 
-    def container_is_running?(server)
+    def pg_container_is_running?(server)
       (capture "docker", "inspect",
         "--format='{{.State.Running}}'",
         server.container_name).strip == "true"
     end
 
-    def file_exists?(file_name_path)
+    def pg_file_exists?(file_name_path)
       test "[", "-f", file_name_path, "]"
     end
 
-    def confirm_file_overwrite?(server, dump_file)
+    def pg_confirm_file_overwrite?(server, dump_file)
       warn "A file named #{dump_file} already exists on #{server.domain} in /tmp. If you continue, you will overwrite it."
       warn "Are you positive(Y/N)?"
       STDOUT.flush
@@ -489,11 +492,11 @@ namespace :pg do
         false
       else
         warn "Please enter Y or N"
-        confirm_file_overwrite?(server, dump_file)
+        pg_confirm_file_overwrite?(server, dump_file)
       end
     end
 
-    def confirm_database_overwrite?(server, database)
+    def pg_confirm_database_overwrite?(server, database)
       warn "There is already data in #{database.name} on #{server.domain} in the container " +
         "#{server.container_name} which stores it's data in #{server.data_path} on the host."
       warn "If you continue, you must be positive you want to overwrite the existing data."
@@ -506,11 +509,11 @@ namespace :pg do
         false
       else
         warn "Please enter Y or N"
-        confirm_database_overwrite?(server, database)
+        pg_confirm_database_overwrite?(server, database)
       end
     end
 
-    def database_empty?(cluster, server, database)
+    def pg_database_empty?(cluster, server, database)
       test("docker", "run", "--rm", "--volumes-from", server.container_name,
         "--entrypoint", "/bin/bash", cluster.image.name, "-lc",
         "'/usr/bin/psql", "-U", "postgres", "-d", database.name,
--- postgresinator.gemspec
+++ postgresinator.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'postgresinator'
-  s.version     = '0.0.2'
-  s.date        = '2014-10-04'
+  s.version     = '0.0.3'
+  s.date        = '2014-10-30'
   s.summary     = "Deploy PostgreSQL"
   s.description = "An Opinionated PostgreSQL Deployment gem"
   s.authors     = ["david amick"]
